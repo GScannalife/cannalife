9533e7ee2bccc83286d00e035249aa50de0f693e
GScannalife <147278987+GScannalife@users.noreply.github.com>
2023-10-13 03:29:43 -0400

    Contact Adj

--- components/contact/ContactForm.jsx
+++ components/contact/ContactForm.jsx
@@ -42,32 +42,79 @@ const ContactForm = () => {
     }
   };
 
+
   return (
     <div>
       {showConfetti && <Confetti />}
       {isLoading && <div>Loading...</div>} 
       {notification && <div>{notification}</div>}
 
-      <div className="form-style-one" data-aos="fade-up">
-        <form onSubmit={handleSubmit}>
-          <div className="messages" />
-          <div className="row controls">
-            {/* ... the rest of your form fields ... */}
-            <div className="col-12">
-              <button className="btn-twentyOne fw-500 tran3s d-block">
-                Send Message
-              </button>
+
+
+    <div className="form-style-one" data-aos="fade-up">
+      <form onSubmit={handleSubmit}>
+        <div className="messages" />
+        <div className="row controls">
+          <div className="col-12">
+            <div className="input-group-meta form-group mb-30">
+              <input
+                type="text"
+                placeholder="Your Name*"
+                name="name"
+                required="required"
+                data-error="Name is required."
+              />
+              <div className="help-block with-errors" />
             </div>
           </div>
-          <div className='hide-everything'>
-            <ReCAPTCHA
-              ref={recaptchaRef}
-              size="invisible"
-              sitekey="6LfIZYsnAAAAAEGYUS93Ugn-_mXssjMcKQV7_47Y" />
+          {/* End .col-12 */}
+
+          <div className="col-12">
+            <div className="input-group-meta form-group mb-50">
+              <input
+                type="email"
+                placeholder="Email Address*"
+                name="email"
+                required="required"
+                data-error="Valid email is required."
+              />
+              <div className="help-block with-errors" />
+            </div>
           </div>
-        </form>
-      </div>
-    </div>
+          {/* End .col-12 */}
+
+          <div className="col-12">
+            <div className="input-group-meta form-group mb-30">
+              <textarea
+                placeholder="Your message*"
+                name="message"
+                required="required"
+                data-error="Please, leave us a message."
+                defaultValue={""}
+              />
+              <div className="help-block with-errors" />
+            </div>
+          </div>
+          {/* End .col-12 */}
+
+          <div className="col-12">
+            <button className="btn-twentyOne fw-500 tran3s d-block">
+              Send Message
+            </button>
+          </div>
+          {/* End .col-12 */}
+          
+        </div>
+        {/* End .row */}
+        <div className='hide-everything'>
+        <ReCAPTCHA
+        ref={recaptchaRef}
+        size="invisible"
+        sitekey="6LfIZYsnAAAAAEGYUS93Ugn-_mXssjMcKQV7_47Y" /></div>
+      
+      </form>
+      
+    </div></div>
   );
 };
 
